b6aba29db488ac71d14695c0551f71de7933f87d
stella <stella2837@naver.com>
2022-11-22 21:56:53 +0900

    add PersonalConfig Activity

--- app/src/main/java/com/example/smart_cage_android/MainActivity.java
+++ app/src/main/java/com/example/smart_cage_android/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_id;
+    private Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         tv_id = findViewById(R.id.tv_id);
+        btn_test = findViewById(R.id.btn_test);
 
         Intent intent = getIntent();
         String userID = intent.getStringExtra("userID");
 
         tv_id.setText(userID + "님 환영합니다");
+
+        //테스트 버튼 클릭
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PersonalConfigActivity.class);
+                intent.putExtra("userID", userID);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
