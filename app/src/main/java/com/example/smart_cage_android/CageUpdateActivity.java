9770ab7199394a8b4d10133e4053e06ad1f0686b
stella <stella2837@naver.com>
2022-12-03 14:48:49 +0900

    edit CageManagement

---
+++ app/src/main/java/com/example/smart_cage_android/CageUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_cage_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CageUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cage_update);
+    }
+}
